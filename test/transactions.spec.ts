95cd7fddd62e11b6f0471899f7db2499fc1c1a0e
WilliamWJD <william.jose.wjd@gmail.com>
2024-07-10 18:06:04 -0300

    feat: configurando vitest

---
+++ test/transactions.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+
+import { app } from '../src/app'
+import { describe } from 'node:test'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('it should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'new transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
